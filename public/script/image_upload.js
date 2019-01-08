1f8757013c2b822fae4d18aaa28e6d47e638ec10
Irevall <kubajmarek@gmail.com>
2019-01-08 01:54:35 +0100

    Fix cropping bug

--- public/script/image_upload.js
+++ public/script/image_upload.js
@@ -10,22 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(`height: ${image.height}`);
         canvas.width = 184;
         canvas.height = 184;
-        canvas.style.width = '184px';
-        canvas.style.height = '184px';
+        ctx.fillStyle = "#FF0000";
 
         const difference_x = window.getComputedStyle(selection).left.slice(0, -2) - image.getBoundingClientRect().left;
         const difference_y = window.getComputedStyle(selection).top.slice(0, -2) - image.getBoundingClientRect().top;
 
-        // console.log(difference_x);
-        // console.log(difference_y);
-        // console.log(window.getComputedStyle(selection).left.slice(0, -2));
-        // console.log(image.getBoundingClientRect().left);
-
         const selectionSize = window.getComputedStyle(selection).width.slice(0, -2);
         console.log(selectionSize);
-
-        // ctx.drawImage(image, difference_x, difference_y, selectionSize, selectionSize, 0, 0, 184, 184);
-        ctx.drawImage(image, 0, 0, 450, 450, 0, 0, 184, 184);
+        console.log(image.width);
+        
+        const scale = (image.naturalWidth / image.width);
+        ctx.drawImage(image, difference_x * scale, difference_y * scale, selectionSize * scale, selectionSize * scale, 0, 0, 184, 184);
         document.querySelector('#test').innerHTML = '';
         document.querySelector('#test').appendChild(canvas);
     }
